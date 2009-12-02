98194a7ad63c534547228f53473deb44fa3697ca
Ryan Garver <ragarver@gmail.com>
2009-12-02 13:36:52 -0800

    Add some missing Central time MI zipcodes

--- spec/zip_to_timezone_spec.rb
+++ spec/zip_to_timezone_spec.rb
@@ -8,4 +8,9 @@ describe "ZipToTimezone" do
   it "should show that DC is Eastern Time" do
     ZipToTimezone.get_timezone_for('20001').should == 'Eastern Time (US & Canada)'
   end
+  
+  it "should know that some of MI is in Central time" do
+    ZipToTimezone.get_timezone_for('49915').should == 'Central Time (US & Canada)'
+    ZipToTimezone.get_timezone_for('49914').should == 'Eastern Time (US & Canada)'
+  end
 end
